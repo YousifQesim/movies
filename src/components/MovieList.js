760fa8b9fd2fea8a31f5b2164e1822e6f5a23b87
{yusifqasim} <{yusifqasim22@gmail.com>
2023-02-04 22:08:03 +0300

    the MovieList Created

---
+++ src/components/MovieList.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+import "./Movies.css"
+export default function MovieList() {
+  const [movies, setMovies] = useState([ {
+    "Title": "Star Wars: Episode IV - A New Hope",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
+  },
+  {
+    "Title": "Star Wars: Episode V - The Empire Strikes Back",
+    "Year": "1980",
+    "imdbID": "tt0080684",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+  },
+  {
+    "Title": "Star Wars: Episode VI - Return of the Jedi",
+    "Year": "1983",
+    "imdbID": "tt0086190",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+  },
+  {
+    "Title": "Star Wars: Episode VII - The Force Awakens",
+    "Year": "2015",
+    "imdbID": "tt2488496",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
+  }]);
+  console.log(movies)
+
+  return (
+
+
+    <div className='movies'>
+    {    movies.map((movie,index)=>(
+      <div >
+        <img src={movie.Poster} alt="movieImg" />
+
+      </div>
+    ))
+}
+    </div>
+  )
+}
