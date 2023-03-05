9a3751f6354e609e4c8476577dfa076f8c6d0db6
{yusifqasim} <{yusifqasim22@gmail.com>
2023-03-05 15:33:22 +0300

    api error solution check

--- src/core.js
+++ src/core.js
@@ -1,8 +1,15 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import {useContext} from 'react'
 
+import Usecontext, { context } from '../src/hooks/Usecontext';
+const { createProxyMiddleware } = require('http-proxy-middleware');
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const {category,cartIteam,} = useContext(context);
+
+const api=`https://api.themoviedb.org/3/movie/${category}/?api_key=e7b12004b75308c7c4a6e84c00d2477e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${cartIteam}&with_watch_monetization_types=flatrate`
 module.exports = function(app) {
   app.use(
-    'https://api.themoviedb.org/3/movie/popular/?api_key=e7b12004b75308c7c4a6e84c00d2477e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate',
+    
+    `${api}`,
     createProxyMiddleware({
       target: 'http://localhost:3000',
       changeOrigin: true,
